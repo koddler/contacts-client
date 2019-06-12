aaa141645098a8db201181e96ab276578d80cb87
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-06-13 01:34:09 +0600

    Add Contacts component
    * Clean up App.js
    * Remove logo.svg

---
+++ src/components/Contatcs.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class Contacts extends PureComponent {
+  render() {
+    return (
+      <div className="container">
+        <p>All your contacts are here.</p>
+      </div>
+    );
+  }
+}
+
+export default Contacts;
