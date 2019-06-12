88a2218daad0e3d71783b87d64770910015978e3
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-06-13 02:29:31 +0600

    Add fake data to Contacts component

--- src/components/Contatcs.jsx
+++ src/components/Contatcs.jsx
@@ -1,6 +1,50 @@
 import React, { PureComponent } from 'react';
 
 class Contacts extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: [],
+      contactDetails: {
+        name: 'N/A',
+        phone: 'N/A',
+        email: 'N/A'
+      }
+    };
+    this.setContacts = this.setContacts.bind(this);
+    this.setDetails = this.setDetails.bind(this);
+  }
+
+  fakeGroup = {
+    groupId: 1,
+    name: 'Default',
+    contacts: [
+      {
+        contactId: 1,
+        name: 'Shaphil',
+        phone: '01911281911',
+        email: 'shaphil2025@gmail.com',
+        groupId: 1
+      },
+      {
+        contactId: 2,
+        name: 'Mahmud',
+        phone: '01678034104',
+        email: 'shaphil.mahmud@gmail.com',
+        groupId: 1
+      }
+    ]
+  };
+
+  setContacts(contacts) {
+    this.setState({ contacts: contacts });
+  }
+
+  setDetails(details) {
+    this.setState({ contactDetails: details });
+  }
+
   render() {
     return (
       <div className="container">
@@ -13,25 +57,31 @@ class Contacts extends PureComponent {
         <div className="row">
           <div className="col-sm">
             <ul className="list-group">
-              <a href="#" className="list-group-item list-group-item-action">Group 1</a>
-              <a href="#" className="list-group-item list-group-item-action">Group 2</a>
-              <a href="#" className="list-group-item list-group-item-action">Group 3</a>
-              <a href="#" className="list-group-item list-group-item-action">Group 4</a>
-              <a href="#" className="list-group-item list-group-item-action">Group 5</a>
+              <li
+                onClick={() => this.setContacts(this.fakeGroup.contacts)}
+                className="list-group-item list-group-item-action"
+              >
+                {this.fakeGroup.name}
+              </li>
             </ul>
           </div>
           <div className="col-sm">
             <ul className="list-group">
-              <li className="list-group-item">Contact 1</li>
-              <li className="list-group-item">Contact 2</li>
-              <li className="list-group-item">Contact 3</li>
-              <li className="list-group-item">Contact 4</li>
-              <li className="list-group-item">Contact 5</li>
+              {this.state.contacts.map(contact => (
+                <li
+                  key={contact.contactId}
+                  onClick={() => this.setDetails(contact)}
+                  className="list-group-item list-group-item-action"
+                >
+                  {contact.name}
+                </li>
+              ))}
             </ul>
           </div>
-          <div className="col-sm border">
-            Contact 1 Details
-
+          <div className="col-sm mx-auto border">
+            <p>{this.state.contactDetails.name}</p>
+            <p>{this.state.contactDetails.phone}</p>
+            <p>{this.state.contactDetails.email}</p>
           </div>
         </div>
       </div>
