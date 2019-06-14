6e2e400223a939d3c21a644c76cc4343e23f6798
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-06-14 16:12:59 +0600

    Add redux form reducer to rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import groupsReducer from './groupsReducer';
 
 export default combineReducers({
+  form: formReducer,
   groupsReducer: groupsReducer
 });
