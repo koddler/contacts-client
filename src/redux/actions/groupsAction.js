a98d23df116936ad00143740512638ee98320ca9
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-06-14 16:10:55 +0600

    Add action addGroup()

--- src/redux/actions/groupsAction.js
+++ src/redux/actions/groupsAction.js
@@ -1,8 +1,13 @@
 import { _get, _post, _put, _delete } from '../../utilities/http';
 
+export const ADD_GROUP = 'ADD_GROUP';
 export const FETCH_GROUPS = 'FETCH_GROUPS';
 
 const GROUPS_URL = 'http://localhost:5000/api/groups';
+
+export const addGroup = group => dispatch => {
+  _post(GROUPS_URL, dispatch, ADD_GROUP, group);
+};
 
 export const fetchGroups = () => dispatch => {
   _get(GROUPS_URL, dispatch, FETCH_GROUPS);
